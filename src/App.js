4a327fe155335a0c3bd46d1eecac5903cdf6f0d6
luiz <lguilhermefl@gmail.com>
2022-10-10 13:54:25 -0300

    feat: add page components and routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import UploadBox from "./components/UploadBox";
-import React from "react";
 import GlobalStyle from "./components/styles/globalStyles";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Home from "./pages/Home";
+import EditTest from "./pages/EditTest";
+import EditAppointment from "./pages/EditAppointment";
+import Appointment from "./pages/Appointment";
+import Test from "./pages/Test";
 
 export default function App() {
   return (
-    <>
+    <BrowserRouter>
       <GlobalStyle />
-      <UploadBox />
-    </>
+      <Routes>
+        <Route path="/" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/history" element={<Home />} />
+        <Route path="/tests/edit/:id" element={<EditTest />} />
+        <Route path="/appointments/edit/:id" element={<EditAppointment />} />
+        <Route path="/appointments/:id" element={<Appointment />} />
+        <Route path="/tests/:id" element={<Test />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
